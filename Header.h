a457daa235d71292d97b32fc1bff9e97b4883bd1
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-06-22 12:43:52 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,53 +1,124 @@
-#pragma once
-#include"Macros.h"
 #include<iostream>
 #include<algorithm>
 #include<type_traits>
-#include<new>
-#include<cstdlib>
-#include<utility>
 #include<initializer_list>
+#include<utility>
 #include<functional>
-#if __cplusplus > 202002L
+#include<new>
+#include"Macros.h"
+
 _PANAGIOTIS_BEGIN
 template<typename _Ty>
-class Queue final {
+class queue final {
 private:
-	class Queue_Node final {
+	class queue_node final {
+	private:
+		class secret_class {};
+		template<class ..._Valty>
+		queue_node(secret_class, _Valty &&..._Val):data{std::forward<_Valty>(_Val)...},next{nullptr}
+		{
+		}
 	public:
 		_Ty data;
-		Queue_Node* next;
-		
-		Queue_Node()noexcept :data{},next{nullptr}
+		queue_node* next;
+		queue_node() :data{}, next{ nullptr }
 		{
 
 		}
-		Queue_Node(const _Ty& item)noexcept(noexcept(data=item))
-			:next{nullptr}
+		queue_node(const _Ty& item)noexcept(noexcept(data=item)) :next{nullptr}
 		{
+			
 			data = item;
 		}
-		Queue_Node( _Ty&& item) noexcept(noexcept(data =std::move( item)))
-			:next{nullptr}
+		queue_node(_Ty&& item)noexcept(noexcept(data=std::move(item))) :next{ nullptr }
 		{
-			data =std::move(item);
+			data = std::move(item);
 		}
-		
+		template<class ..._Valty>
+		static queue_node* craft(_Valty&&..._Val) {
+			queue_node* ptr = new (std::nothrow)queue_node(secret_class{}, std::forward<_Valty>(_Val)...);
+			return ptr;
+		}
 	};
 	std::size_t count;
-	Queue_Node* head;
-	Queue_Node* tail;
+	queue_node* head;
+	queue_node* tail;
+	template<typename _Valty>
+	bool push_node(_Valty &&_Val) {
+		queue_node* ptr = new (std::nothrow)queue_node(std::forward<_Valty>(_Val));
+		if (ptr != nullptr) {
+			if (count != 0) {
+				tail->next = ptr;
+				tail = ptr;
+			}
+			else {
+				head = tail = ptr;
+				
+			}
+			count++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	void pop_node() {
+		if (count != 0) {
+			queue_node* ptr = head;
+			head = head->next;
+			count--;
+			delete ptr;
+			if (head==nullptr)tail = nullptr;
+			return;
+		}
+		throw pop_from_an_empty_queue_{ "tried to pop from an empty queue" };
+	}
+	void swap_queue(queue<_Ty>& other) {
+		std::swap(head, other.head);
+		std::swap(count, other.count);
+		std::swap(tail, other.tail);
+	}
+	void clear()noexcept {
+		static_assert(std::is_destructible_v<_Ty>, "the type must be destructible");
+		while (head != nullptr) {
+			queue_node* ptr{ head };
+			head = head->next;
+			delete ptr;
+		}
+		head = tail = nullptr;
+		count = 0;
+	}
+	template<typename..._Valty>
+	bool emplace_node(_Valty&&..._Val) {
+		queue_node* ptr = queue_node::craft(std::forward<_Valty>(_Val)...);
+		if (ptr != nullptr) {
+			if (count != 0) {
+				tail->next = ptr;
+				tail = ptr;
+			}
+			else {
+				head = tail = ptr;
+
+			}
+			count++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 public:
 	static_assert(std::is_object_v<_Ty>, "The C++ Standard forbids container adaptors of non-object types "
 		"because of [container.requirements].");
 	static_assert(!std::is_reference_v<_Ty>, "no references allowed");
 	static_assert(!std::is_const_v<_Ty>, "no const types are allowed");
 	static_assert(!std::is_volatile_v<_Ty>, "no volatile types are allowed");
-	Queue()noexcept :count{0},head{nullptr},tail{nullptr}
-	{//contructor
+	static_assert(!std::is_array_v<_Ty>, "no raw c arrays are allowed");
+	queue()noexcept :head{ nullptr }, tail{ nullptr }, count{}
+	{
 
 	}
-	Queue(const std::initializer_list<_Ty>& l) : head{ nullptr }, tail{nullptr}, count{ 0 }
+	queue(const std::initializer_list<_Ty>& l) :head{ nullptr }, tail{ nullptr }, count{ 0 }
 	{
 		const _Ty* b = l.begin();
 		for (std::size_t i = 0; i < l.size(); i++) {
@@ -55,123 +126,56 @@ public:
 				b++;
 			}
 			else {
-				this->~Queue();
+				this->~queue();
 				break;
 			}
 
 		}
 	}
-
-	Queue(const Queue<_Ty>& other) :count{0}, head{nullptr},tail{nullptr}
+	queue(const queue<_Ty>& other):head{nullptr},tail{nullptr},count{}
 	{
-		if (this != &other) {
-			if (other.count != 0) {//exei na mas dosei kati o allos 
-				Queue_Node* ptr = other.head;
-				head = new (std::nothrow)Queue_Node(ptr->data);
-				if (head != nullptr) {
-					tail = head;
-					count++;
-					
-					for (std::size_t i = 1; i < other.count; i++) {
-						tail->next = new (std::nothrow)Queue_Node(ptr->next->data);
-						if (tail->next != nullptr) {
-							ptr = ptr->next;
-							tail = tail->next;
-							count++;
-						}
-						else {
-							this->~Queue();
-							break;
-						}
-					}
-				}
-				
-			}
-		}
-	}
-	Queue (Queue<_Ty>&& other)noexcept :count{ 0 }, head{ nullptr }, tail{ nullptr }
-	{
-		if (this != &other) {
-			head = other.head;
-			tail = other.tail;
-			count = other.count;
-			other.head = other.tail = nullptr;
-			other.count = 0;
-		}
-	}
-
-	bool push(const _Ty&item) 
-	{
-		Queue_Node* ptr = new (std::nothrow)Queue_Node(item);
-		if (count != 0) {
-			
-			if (ptr != nullptr) {
-				tail->next = ptr;
-				tail = tail->next;
-				count++;
-				return true;
-			}
-
-
-		}
-		else {
-			if (ptr != nullptr) {
-				head = tail = ptr;
-				count++;
-				return true;
-			}
-		}
-		return false;
-	}
-	bool push(_Ty&& item)
-	{
-		Queue_Node* ptr = new (std::nothrow)Queue_Node(std::move(item));
-		if (count != 0) {
-
-			if (ptr != nullptr) {
-				tail->next = ptr;
-				tail = tail->next;
-				count++;
-				return true;
-			}
-
-
-		}
-		else {
-			if (ptr != nullptr) {
-				head = tail = ptr;
-				count++;
-				return true;
-			}
-		}
-		return false;
-	}
-	void pop() 
-	{
-		if (count != 0) {
-			Queue_Node* ptr = head;
-			head = head->next;
-			if (count == 1) {
-				tail = nullptr;
-
-			}
-			delete ptr;
-			count--;
 		
+		queue_node* ptr{ other.head };
+		while (ptr != nullptr) {
+			if (push(ptr->data)) {
+				ptr = ptr->next;
+			}
+			else {
+				clear();
+				break;
+			}
 		}
-		else {
-			throw pop_from_an_empty_queue_{ "tried to pop from an empty queue" };
-
-		}
+		
+	}
+	queue(queue<_Ty>&& other)noexcept :head{ nullptr }, tail{ nullptr }, count{} {
+		std::swap(head, other.head);
+		std::swap(tail, other.tail);
+		std::swap(count, other.count);
+	}
+	bool push(const _Ty& data) {
+		return push_node(data);
+	}
+	bool push(_Ty&& data) {
+		return push_node(std::move(data));
+	}
+	
+	bool empty()const noexcept {
+		return count == 0;
+	}
+	std::size_t size()const noexcept {
+		return count;
+	}
+	void pop() {
+		pop_node();
 	}
 	_NODISCARD _Ty back()&& {
 		if (count == 0) {
-			throw tried_to_access_an_empty_queue_{"tried to access from an empty queue "};
+			throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
 		}
 		return std::move(tail->data);
-		
+
 	}
-	_NODISCARD const _Ty back()const && {
+	_NODISCARD const _Ty back()const&& {
 		if (count == 0) {
 			throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
 		}
@@ -193,234 +197,115 @@ public:
 
 	}
 	_NODISCARD const _Ty& front()const& {
-		 if (count == 0) {
-			 throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
-		 }
-		 return head->data;
+		if (count == 0) {
+			throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
+		}
+		return head->data;
 
 	}
 	_NODISCARD _Ty& front()& {
-		 if (count == 0) {
-			 throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
-		 }
-		 return head->data;
+		if (count == 0) {
+			throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
+		}
+		return head->data;
 
 	}
 	_NODISCARD _Ty front()&& {
-		 if (count == 0) {
-			 throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
-		 }
-		 return std::move(head->data);
+		if (count == 0) {
+			throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
+		}
+		return std::move(head->data);
 
 	}
 	_NODISCARD _Ty front()const&& {
-		 if (count == 0) {
-			 throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
-		 }
-		 return std::move(head->data);
-
-	}
-
-	_NODISCARD bool empty() const noexcept
-	{
-		return count == 0;
-	}
-
-	_NODISCARD std::size_t size() const noexcept
-	{
-		return count;
-	}
-	void swap(Queue<_Ty>& other)noexcept
-	{
-		std::swap(head, other.head);
-		std::swap(tail, other.tail);
-		std::swap(count, other.count);
-
-	}
-
-	Queue<_Ty>& operator =(const Queue<_Ty>& other)&
-	{//copy constructor 
-		if (this == &other)return *this;
-		if (other.count != 0) {
-			if (count == 0) {
-				Queue_Node* ptr = other.head;
-				head = new (std::nothrow)Queue_Node(ptr->data);
-				if (head != nullptr) {
-					tail = head;
-					count++;
-
-					for (std::size_t i = 1; i < other.count; i++) {
-						tail->next = new (std::nothrow)Queue_Node(ptr->next->data);
-						if (tail->next != nullptr) {
-							ptr = ptr->next;
-							tail = tail->next;
-							count++;
-						}
-						else {
-							this->~Queue();
-							break;
-						}
-					}
-				}
-				return *this;
-			}
-			else {
-
-				Queue_Node* ptr1 = head;
-				Queue_Node* ptr2 = other.head;
-				while (ptr1 != nullptr && ptr2 != nullptr) {
-
-					while (ptr1->next != nullptr && ptr2->next != nullptr) {
-						ptr1->data = ptr2->data;
-						ptr1 = ptr1->next;
-						ptr2 = ptr2->next;
-					}
-					ptr1->data = ptr2->data;
-					break;
-
-				}
-				/*if (count == other.count) {
-					Queue_Node* ptr1 = head;
-					Queue_Node* ptr2 = other.head;
-					while (ptr1 != nullptr && ptr2 != nullptr) {
-						ptr1->data = ptr2->data;
-						ptr1 = ptr1->next;
-						ptr2 = ptr2->next;
-
-					}
-					return *this;
-				}*/
-				if (count < other.count) {
-					/*Queue_Node* ptr1 = head;
-					Queue_Node* ptr2 = other.head;
-					while (ptr1 != nullptr && ptr2 != nullptr) {
-
-						while (ptr1->next != nullptr && ptr2->next != nullptr) {
-							ptr1->data = ptr2->data;
-							ptr1 = ptr1->next;
-							ptr2 = ptr2->next;
-						}
-						ptr1->data = ptr2->data;
-						break;
-
-					}*/
-					ptr2 = ptr2->next;
-					while (ptr2 != nullptr) {
-						if (push(ptr2->data)) {
-							ptr2 = ptr2->next;
-
-						}
-						else {
-							this->~Queue();
-							break;
-						}
-						
-
-					}
-					return *this;
-				}
-				else if (count > other.count) {
-					/*Queue_Node* ptr1 = head;
-					Queue_Node* ptr2 = other.head;
-					while (ptr1 != nullptr && ptr2 != nullptr) {
-
-						while (ptr1->next != nullptr && ptr2->next != nullptr) {
-							ptr1->data = ptr2->data;
-							ptr1 = ptr1->next;
-							ptr2 = ptr2->next;
-						}
-						ptr1->data = ptr2->data;
-						break;
-
-					}*/
-					tail = ptr1;
-					ptr1 = ptr1->next;
-					while (ptr1 != nullptr) {
-						
-						Queue_Node* ptr = ptr1;
-						ptr1 = ptr1->next;
-						delete ptr;
-						count--;
-					}
-					tail->next=nullptr;
-					return *this;
-
-				}
-				return *this;
-			}
-
+		if (count == 0) {
+			throw tried_to_access_an_empty_queue_{ "tried to access from an empty queue " };
 		}
-		else {
-			this->~Queue();
-			return *this;
-		}
-		
-	}
+		return std::move(head->data);
 
-	Queue<_Ty>& operator =(Queue<_Ty>&& other)&noexcept
-	{
-		this->~Queue();
-		if (other.count != 0) {
-			head = other.head;
-			tail = other.tail;
-			count = other.count;
-			other.head = other.tail = nullptr;
-			other.count = 0;
-		}
-		return *this;
 	}
-	~Queue()noexcept 
-	{
-		for (std::size_t i = 0; i < count; i++) {
-			
-			Queue_Node* ptr = head;
-			head = head->next;
-			delete ptr;
-		}
-		count = 0;
-		head = tail = nullptr;
+	void swap(queue<_Ty>& other)noexcept {
+		swap_queue(other);
+
 	}
-	void reverse() noexcept 
-	{
-		if (count > 1) {
-			Queue_Node* ptr1 = head;
-			Queue_Node* ptr2 = head->next;
-			Queue_Node* newHead = nullptr;
-			std::size_t count1 = 0;
-			while (ptr2 != nullptr) {
-				if (count1 == 0) {
-					count1++;
-					Queue_Node* ptr = ptr2->next;
-					ptr2->next = ptr1;
-					ptr1->next = ptr;
-					newHead = ptr2;
-					ptr2 = ptr1->next;
+	~queue()noexcept {
+		clear();
+	}
+	template<typename ..._Valty>
+	bool emplace(_Valty&&..._Val) {
+		return emplace_node(std::forward<_Valty>(_Val)...);
+	}
+	queue<_Ty>& operator =(const queue<_Ty>& other)& {
+		queue_node* prev1{ nullptr };
+		queue_node* prev2{ nullptr };
+		queue_node* curr1{ head };
+		queue_node* curr2{ other.head };
+		while (curr1 != nullptr && curr2 != nullptr) {
+			curr1->data = curr2->data;
+			prev1 = curr1;
+			prev2 = curr2;
+			curr1 = curr1->next;
+			curr2 = curr2->next;
+		}
+		if (prev1 == nullptr && curr2 != nullptr || prev1 != nullptr && curr2 != nullptr) {
+			//size<other.size
+			while (curr2 != nullptr) {
+				if (push(curr2->data)) {
+					curr2 = curr2->next;
 				}
 				else {
-					Queue_Node* ptr = ptr2->next;
-					ptr2->next = newHead;
-					ptr1->next = ptr;
-					newHead = ptr2;
-					ptr2 = ptr1->next;
+					clear();
+					break;
 				}
-
-
-
 			}
-			head = newHead;
-			tail = newHead;
-			if (tail == nullptr)return;
-			for (std::size_t i = 0; i < count-1; i++) {
-				tail = tail->next;
-			}
+			return *this;
+		}
+		if (prev2 == nullptr && curr1 != nullptr) {
+			clear();
+			return *this;
 			
 		}
+		if (curr1 != nullptr) {//size>other.size,throw extras
+			tail = prev1;
+			tail->next = nullptr;
+			while (curr1 != nullptr) {
+				prev1 = curr1;
+				curr1 = curr1->next;
+				delete prev1;
+				count--;
+			}
+
+			return *this;
+		}
+		//if (curr2 != nullptr) {//size<other.size,allocate extra
+		//	while (curr2 != nullptr) {
+		//		if (push(curr2->data)) {
+		//			curr2 = curr2->next;
+		//		}
+		//		else {
+		//			clear();
+		//			break;
+		//		}
+		//	}
+		//	return *this;
+		//}
+		return *this;
 	}
-	void clear()noexcept{
-		this->~Queue();
+	queue<_Ty>& operator=(queue<_Ty>&& other) &noexcept{
+		clear();
+		std::swap(head, other.head);
+		std::swap(count, other.count);
+		std::swap(tail, other.tail);
+		return *this;
+
 	}
-	
+	void show() {
+		queue_node* ptr{ head };
+		while (ptr != nullptr) {
+			std::cout << ptr->data << '\n';
+			ptr = ptr->next;
+		}
+	}
 
 };
 _PANAGIOTIS_END
-#endif
